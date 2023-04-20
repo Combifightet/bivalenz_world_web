f2262d409b0e510bfc2c707649547d6689722488
Combifightet <malte@hei-sharp-s.de>
2023-04-20 23:11:39 +0200

    scalable grid

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
-const Color col = Color(0xff23272e);
+const Color backgroundColor = Color(0xff23272e);
+const Color backgroundAccentColor = Color(0xff2c313c);
+
+const Color foregroundColor = Color(0xffabb2bf);
+const Color foregroundAccentColor = Color(0xff7f848e);
+
+const Color redAccentColor = Color(0xffe06c75);
+const Color greenAccentColor = Color(0xff98c379);
+const Color blueAccentColor = Color(0xff61afef);
+const Color yellowAccentColor = Color(0xffe5c07b);
+
+const Color orangeAccentColor = Color(0xffd19a66);
+const Color cyanAccentColor = Color(0xff56b6c2);
+const Color purpleAccentColor = Color(0xffc678dd);
