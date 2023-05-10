757cf356973eb04876f54caa6c2e3e5a3ea2f26a
Combifightet <malte@hei-sharp-s.de>
2023-05-10 12:08:01 +0200

    logic handeling ui finished

--- lib/logic_handeling.dart
+++ lib/logic_handeling.dart
@@ -2,122 +2,93 @@ import 'package:flutter/material.dart';
 
 import 'theme.dart';
 
-class LogicObj extends StatefulWidget {
-  const LogicObj({super.key});
-
-  @override
-  State<LogicObj> createState() => LogicObjState();
-}
-
-class LogicObjState extends State<LogicObj> {
+class LogicObj extends StatelessWidget {
+  LogicObj({super.key});
   final _controller = TextEditingController();
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(0),
-            child: AspectRatio(
-              aspectRatio: 1,
-              child: Card(
-                color: backgroundAccentColor,
-                elevation: 5,
-                child: FittedBox(
-                  fit: BoxFit.contain,
-                  // child: Text('T', style: TextStyle(fontWeight: FontWeight.bold, color: greenAccentColor)),
-                  // child: Text('F', style: TextStyle(fontWeight: FontWeight.bold, color: redAccentColor)),
-                  // child: Icon(Icons.star, color: foregroundAccentColor),
-                  child: Icon(Icons.close_rounded, color: foregroundAccentColor),
-                ),
-              )
-            )
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(3),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: backgroundAccentColor,
-                  hintText: 'Enter a logic statement',
-                  hintStyle: TextStyle(color: foregroundAccentColor.withOpacity(.5)),
-                  border: UnderlineInputBorder(borderSide: BorderSide(color: foregroundAccentColor.withOpacity(0.5))) // TODO: Does not yet display the correct coler for the underline of the text field
-                ),
-                style: const TextStyle(
-                  color: foregroundAccentColor,
-                ),
-                controller: _controller,
-                onChanged: (value) {
-                  int cursorOffset = _controller.selection.baseOffset;
-                  String formattedInput = value.replaceAll('!', '¬');           // logical not
-                  cursorOffset -= RegExp('->').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
-                  formattedInput = formattedInput.replaceAll('->', '→');        // logical implication
-                  cursorOffset -= 2 * RegExp('<=>').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
-                  formattedInput = formattedInput.replaceAll('<=>', '⇔');      // logical equivalence
-                  cursorOffset -= RegExp('¬=').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
-                  formattedInput = formattedInput.replaceAll('¬=', '≠');        // not equal
-                  cursorOffset -= RegExp('¬≠').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
-                  formattedInput = formattedInput.replaceAll('¬≠', '=');        // equal
-                  cursorOffset -= RegExp('_¬').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
-                  formattedInput = formattedInput.replaceAll('_¬', '⊥');        // contradiction (truth value false)
-                  formattedInput = formattedInput.replaceAll('|', '∨');         // logical disjunction (or)
-                  formattedInput = formattedInput.replaceAll('&', '∧');         // logical conjunction (and)
-                  cursorOffset -= RegExp('_V').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
-                  formattedInput = formattedInput.replaceAll('_V', '∀');        // quantification (for all)
-                  cursorOffset -= RegExp('_E').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
-                  formattedInput = formattedInput.replaceAll('_E', '∃');        // quantification (there exists)
-                  
-                  // formattedInput = formattedInput.replaceAll('(', '()');        // autoclose brackets
-                  if (cursorOffset != 0 && formattedInput[cursorOffset-1] == '(') {
-                    if (cursorOffset == value.length) {
-                      formattedInput = '$formattedInput)';
-                    } else {
-                      if (formattedInput[cursorOffset] != ')') {
-                        formattedInput = '${formattedInput.substring(0, cursorOffset)})${formattedInput.substring(cursorOffset)}';
-                      }
-                    }
-                  }
-                  
-                  
-                  _controller.value = TextEditingValue(
-                    text: formattedInput,
-                    selection: TextSelection.collapsed(offset: cursorOffset.clamp(0, value.length+1)),
-                  );
-                },
-              )
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(4),
-            child: AspectRatio(aspectRatio: 1, child:Tooltip(
-              message: 'remove this logic statement',
-              waitDuration: const Duration(seconds: 1),
-              child: TextButton(
-                style: squareButtonStyle,
-                onPressed: () {
-                  debugPrint('delete this logic statement');
-                },
-                child: const FittedBox(
-                  fit: BoxFit.contain,
-                  child: Icon(Icons.delete_outline_rounded),
-                ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(0),
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Card(
+              color: backgroundAccentColor,
+              elevation: 5,
+              child: FittedBox(
+                fit: BoxFit.contain,
+                // child: Text('T', style: TextStyle(fontWeight: FontWeight.bold, color: greenAccentColor)),
+                // child: Text('F', style: TextStyle(fontWeight: FontWeight.bold, color: redAccentColor)),
+                // child: Icon(Icons.star, color: foregroundAccentColor),
+                child: Icon(Icons.close_rounded, color: foregroundAccentColor),
               ),
-            ),
             )
           )
-        ],
-      )
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(3),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: backgroundAccentColor,
+                hintText: 'Enter a logic statement',
+                hintStyle: TextStyle(color: foregroundAccentColor.withOpacity(.5)),
+                border: UnderlineInputBorder(borderSide: BorderSide(color: foregroundAccentColor.withOpacity(0.5))) // TODO: Does not yet display the correct coler for the underline of the text field
+              ),
+              style: const TextStyle(
+                color: foregroundAccentColor,
+              ),
+              controller: _controller,
+              onChanged: (value) {
+                int cursorOffset = _controller.selection.baseOffset;
+                String formattedInput = value.replaceAll('!', '¬');             // logical not
+                cursorOffset -= RegExp('->').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
+                formattedInput = formattedInput.replaceAll('->', '→');          // logical implication
+                cursorOffset -= 2 * RegExp('<=>').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
+                formattedInput = formattedInput.replaceAll('<=>', '⇔');        // logical equivalence
+                cursorOffset -= RegExp('¬=').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
+                formattedInput = formattedInput.replaceAll('¬=', '≠');          // not equal
+                cursorOffset -= RegExp('¬≠').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
+                formattedInput = formattedInput.replaceAll('¬≠', '=');          // equal
+                cursorOffset -= RegExp('_¬').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
+                formattedInput = formattedInput.replaceAll('_¬', '⊥');          // contradiction (truth value false)
+                cursorOffset -= RegExp('¬⊥').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
+                formattedInput = formattedInput.replaceAll('¬⊥', '⊤');          // tautology (truth value true)
+                cursorOffset -= RegExp('¬⊤').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
+                formattedInput = formattedInput.replaceAll('¬⊤', '⊥');          // tautology (truth value true)
+                formattedInput = formattedInput.replaceAll('|', '∨');           // logical disjunction (or)
+                formattedInput = formattedInput.replaceAll('&', '∧');           // logical conjunction (and)
+                cursorOffset -= RegExp('_V').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
+                formattedInput = formattedInput.replaceAll('_V', '∀');          // quantification (for all)
+                cursorOffset -= RegExp('_E').allMatches(formattedInput.substring(0, cursorOffset.clamp(0, value.length))).length;
+                formattedInput = formattedInput.replaceAll('_E', '∃');          // quantification (there exists)
+                
+                cursorOffset = cursorOffset.clamp(0, value.length);
+                
+                if (cursorOffset != 0 && formattedInput[cursorOffset-1] == '(') {
+                  if (cursorOffset == value.length) {
+                    formattedInput = '$formattedInput)';
+                  } else {
+                    if (formattedInput[cursorOffset] != ')') {
+                      formattedInput = '${formattedInput.substring(0, cursorOffset)})${formattedInput.substring(cursorOffset)}';
+                    }
+                  }
+                }
+
+                _controller.value = TextEditingValue(
+                  text: formattedInput,
+                  selection: TextSelection.collapsed(offset: cursorOffset.clamp(0, value.length)),
+                );
+              },
+            )
+          ),
+        ),
+      ],
     );
   }
 }
@@ -135,11 +106,11 @@ class LogicObjList extends StatefulWidget {
 }
 
 class LogicObjListState extends State<LogicObjList> {
-  final List<LogicObj> logicObjs = [];
+  final List<LogicObj> logicObjs = [LogicObj(key: UniqueKey())];
 
   void _addItem() {
     setState(() {
-      logicObjs.add(const LogicObj());
+      logicObjs.add(LogicObj(key: UniqueKey()));
     });
   }
 
@@ -207,7 +178,6 @@ class LogicObjListState extends State<LogicObjList> {
                       style: squareButtonStyle,
                       onPressed: () {
                         _clearItems();
-                        debugPrint('deleted all logic statements');
                       },
                       child: const FittedBox(
                         fit: BoxFit.contain,
@@ -227,7 +197,36 @@ class LogicObjListState extends State<LogicObjList> {
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.all(1),
-                child: logicObjs[index],
+                // child: logicObjs[index],
+                child: Container(
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(child: logicObjs[index]),
+                      Padding(
+                        padding: const EdgeInsets.all(4),
+                        child: AspectRatio(aspectRatio: 1,
+                          child:Tooltip(
+                            message: 'remove this logic statement',
+                            waitDuration: const Duration(seconds: 1),
+                            child: TextButton(
+                              style: squareButtonStyle,
+                              onPressed: () {
+                                _removeItem(index);
+                              },
+                              child: const FittedBox(
+                                fit: BoxFit.contain,
+                                child: Icon(Icons.delete_outline_rounded),
+                              ),
+                            ),
+                          ),
+                        )
+                      )
+                    ],
+                  ),
+                ),
               );
             },
           ),
