5b85f4a719f99a811919db313e0080a9d688d48c
Combifightet <malte@hei-sharp-s.de>
2023-05-12 17:54:44 +0200

    created logic_checking

---
+++ lib/logic_checking.dart
@@ -0,0 +1,8 @@
+import 'package:bivalenz_world_web/object_handeling.dart';
+
+String checkLogicTxt(String logicTxt, LogicBoard logicBoard) {
+  return '⊤';
+  // return '⊥';
+  // return 'error0';
+  // return 'error1';
+}
